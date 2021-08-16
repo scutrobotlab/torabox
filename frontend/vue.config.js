7d55d5ac9c2e72c872204f7fe1226ff098c5aa53
M3chD09 <m3chd09@protonmail.com>
2021-08-16 19:28:30 +0800

    prepare for dev

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  devServer: {
+    proxy: 'http://localhost:8000'
+  },
+
+  outputDir: '../backend/public',
+
+  indexPath: process.env.NODE_ENV === 'production'
+    ? '../resources/views/index.blade.php'
+    : 'index.html',
+
+  productionSourceMap: false,
 }
