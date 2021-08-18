73d3eb795ddccdd809e01eca36c531b0dd358050
M3chD09 <m3chd09@protonmail.com>
2021-08-18 20:32:07 +0800

    backend use session in api

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -40,7 +40,9 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \App\Http\Middleware\EncryptCookies::class,
             'throttle:api',
+            \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
