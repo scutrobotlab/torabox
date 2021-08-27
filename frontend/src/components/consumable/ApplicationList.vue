44bd7b1167d3a1059728fe63ff8ac9606c2aad1a
M3chD09 <m3chd09@protonmail.com>
2021-08-27 10:25:15 +0800

    frontend move to component ApplicationList

---
+++ frontend/src/components/consumable/ApplicationList.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-list two-line>
+    <v-list-item
+      v-for="application in applications"
+      :key="application.id"
+      :to="`/dashboard/application/consumable/${application.id}`"
+    >
+      <v-list-item-content>
+        <v-list-item-title v-text="application.applicant.name"></v-list-item-title>
+        <v-list-item-subtitle>
+          {{ "申请 " + application.number + " " + application.consumable.name }}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+      <v-list-item-action>
+        <v-list-item-action-text v-if="application.status == 1" class="green--text">
+          <v-icon small color="success">mdi-check-circle-outline</v-icon> 已同意
+        </v-list-item-action-text>
+        <v-list-item-action-text v-else-if="application.status == -1" class="red--text">
+          <v-icon small color="error">mdi-close-circle-outline</v-icon> 已拒绝
+        </v-list-item-action-text>
+      </v-list-item-action>
+      <v-list-item-action>
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-list-item-action>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  props: ["applications"],
+};
+</script>
+
+<style></style>
--- frontend/src/views/dashboard/application/immovable/List.vue
+++ frontend/src/views/dashboard/application/immovable/List.vue
@@ -4,42 +4,14 @@
     <WaitProgress v-if="loading" class="ma-7" />
 
     <v-fade-transition>
-      <v-list two-line v-if="!loading">
-        <v-list-item
-          v-for="application in applications"
-          :key="application.id"
-          :to="`/dashboard/application/immovable/${application.id}`"
-        >
-          <v-list-item-content>
-            <v-list-item-title v-text="application.applicant.name"></v-list-item-title>
-            <v-list-item-subtitle>
-              {{
-                "申请 " +
-                  (application.action == "lend" ? "借出" : "还回") +
-                  " " +
-                  application.immovable.name
-              }}
-            </v-list-item-subtitle>
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-list-item-action-text v-if="application.status == 1" class="green--text">
-              <v-icon small color="success">mdi-check-circle-outline</v-icon> 已同意
-            </v-list-item-action-text>
-            <v-list-item-action-text v-else-if="application.status == -1" class="red--text">
-              <v-icon small color="error">mdi-close-circle-outline</v-icon> 已拒绝
-            </v-list-item-action-text>
-          </v-list-item-action>
-          <v-list-item-action>
-            <v-icon>mdi-chevron-right</v-icon>
-          </v-list-item-action>
-        </v-list-item>
-      </v-list>
+      <ApplicationList :applications="applications" v-if="!loading" />
     </v-fade-transition>
   </v-container>
 </template>
 
 <script>
 import WaitProgress from "@/components/WaitProgress.vue";
+import ApplicationList from "@/components/immovable/ApplicationList.vue";
 import errorMixin from "@/mixins/errorMixin.js";
 import { getImmovableApplications } from "@/api/immovable_application.js";
 
@@ -47,6 +19,7 @@ export default {
   mixins: [errorMixin],
   components: {
     WaitProgress,
+    ApplicationList,
   },
   data: () => ({
     loading: true,
